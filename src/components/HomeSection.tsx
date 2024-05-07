d03a5b92362ec381400c2beec8fc4b274a62e59f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-05-07 17:05:56 +0100

    home text update

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -45,15 +45,21 @@ const HomeSection = () => {
     {small && (
               <div>
                 <div className='items-center justify-center mt-20'>
-                <h1 className=' welcomeTextsm text-white text-center'>Welcome to the</h1>
+                  <div className="flex items-center justify-center">
+                    <h1 className=' welcomeTextsm text-white text-center'>Welcome to the</h1>
+                  </div>
                 <div className='flex flex-row items-center justify-center'>
-                  <div className='relative flex flex-col mt-[-35px]'>
-                    <h1 className='homeBigTitlesm text-white text-center'>Home of</h1>
+                  <div className='relative flex flex-col items-center justify-center mt-[-35px]'>
+                    <div className="flex items-center justify-center">
+                      <h1 className='homeBigTitlesm text-white text-center'>Home of</h1>
+                    </div>
+                    <div className="flex items-center justify-center">
                       <img
                       src='./assets/Crown.svg'
                       className='absolute bottom-[68px] left-[150px] h-[59.5px] w-[70px] '
                       />
                       <h1 className=' ml-5 homeBigTitlesm text-white text-center mt-[-50px]'> Memes</h1>
+                    </div>
                   </div>
                 </div>
                 <div className='flex flex-col justify-center items-center'>
