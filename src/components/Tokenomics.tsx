fb70b3e89528899a7a7ca539ae28ad03ee5e810f
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-05-07 10:33:27 +0100

    tokenomics and first project update

--- src/components/Tokenomics.tsx
+++ src/components/Tokenomics.tsx
@@ -7,51 +7,54 @@ const Tokenomics = () => {
   return (
     <div>
         {full &&(
-            <div className=' pt-10'>
-                <div className="relative h-[1450px] mt-6">
-                    <div className="absolute top-0 left-[24%] ">
-                        <div className='tokenomicsBox bg-white p-4 rounded-md'>
-                        <h2 className='tokenomicsHeader text-white'>Tokenomics</h2>
-                        
-                        <div className='grid grid-rows-6 mt-14  border border-black rounded-3xl'>
+            <div className=' pt-10 '>
+                <div className="w-screen h-[1450px] mt-6 items-center justify-center">
+                    <div className=" items-center justify-center flex mt-28">
+                        <div className="absolute">
+                            <div className='tokenomicsBox bg-white p-4 rounded-md'>
+                            <h2 className='tokenomicsHeader text-white'>Tokenomics</h2>
                             
-                        <div className='grid grid-cols-3 border-b border-black mt-3  justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px] mb-2'>Community Fund</p>
-                            <div className='border-r border-black h-[36px] mr-[194px]'></div>
-                            <p className='tokenomicsCol2 ml-[-90px] '>10%</p>
-                            </div>
+                            <div className='grid grid-rows-6 mt-14  border border-black rounded-3xl'>
+                                
+                            <div className='grid grid-cols-3 border-b border-black mt-3  justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px] mb-2'>Community Fund</p>
+                                <div className='border-r border-black h-[36px] mr-[194px]'></div>
+                                <p className='tokenomicsCol2 ml-[-90px] '>10%</p>
+                                </div>
 
-                            <div className='grid grid-cols-3 pt-[14px] pb-[14px] border-b border-black items-center justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px]'>Liquidity Pools</p>
-                            <div className='border-r border-black h-[36px] mr-[194px] '></div>
-                            <p className='tokenomicsCol2 ml-[-90px]'>40%</p>
-                            </div>
+                                <div className='grid grid-cols-3 pt-[14px] pb-[14px] border-b border-black items-center justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px]'>Liquidity Pools</p>
+                                <div className='border-r border-black h-[36px] mr-[194px] '></div>
+                                <p className='tokenomicsCol2 ml-[-90px]'>40%</p>
+                                </div>
 
-                            <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px]'>Presale</p>
-                            <div className='border-r border-black h-[36px] mr-[194px] '></div>
-                            <p className='tokenomicsCol2 ml-[-90px]'>50%</p>
-                            </div>
+                                <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px]'>Presale</p>
+                                <div className='border-r border-black h-[36px] mr-[194px] '></div>
+                                <p className='tokenomicsCol2 ml-[-90px]'>50%</p>
+                                </div>
 
-                            <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px]'>Total Supply</p>
-                            <div className='border-r border-black h-[36px] mr-[194px] '></div>
-                            <p className='tokenomicsCol2 ml-[-90px]'>69000000420</p>
-                            </div>
+                                <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px]'>Total Supply</p>
+                                <div className='border-r border-black h-[36px] mr-[194px] '></div>
+                                <p className='tokenomicsCol2 ml-[-90px]'>69000000420</p>
+                                </div>
 
-                            <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px]'>Token Name</p>
-                            <div className='border-r border-black h-[36px] mr-[194px] '></div>
-                            <p className='tokenomicsCol2 ml-[-90px]'>Home of Memes</p>
-                            </div>
+                                <div className='grid grid-cols-3 border-b border-black items-center justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px]'>Token Name</p>
+                                <div className='border-r border-black h-[36px] mr-[194px] '></div>
+                                <p className='tokenomicsCol2 ml-[-90px]'>Home of Memes</p>
+                                </div>
 
-                            <div className='grid grid-cols-3  border-black items-center justify-center'>
-                            <p className='tokenomicsCol1 mx-[43px]'>Token Ticker</p>
-                            <div className='border-r border-black h-[36px] mr-[194px] '></div>
-                            <p className='tokenomicsCol2 ml-[-90px]'>HOME</p>
+                                <div className='grid grid-cols-3  border-black items-center justify-center'>
+                                <p className='tokenomicsCol1 mx-[43px]'>Token Ticker</p>
+                                <div className='border-r border-black h-[36px] mr-[194px] '></div>
+                                <p className='tokenomicsCol2 ml-[-90px]'>HOME</p>
+                                </div>
+                            </div>
                             </div>
                         </div>
-                        </div>
+
                     </div>
                     <div className="bg-black h-[10%]"></div>
                     <div className=" bg-homeBG h-[90%]">
@@ -59,24 +62,24 @@ const Tokenomics = () => {
                             <div className='flex flex-col items-center justify-center '>
                             <div className='flex'>
                                 <div className='bg-white pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                                <img
-                                src='./assets/CMC.svg'
-                                />
+                                    <img
+                                    src='./assets/CMC.svg'
+                                    />
                                 </div>
                                 <div className='ml-2 bg-white pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                                <img
-                                src='./assets/CGK.svg'
-                                />
+                                    <img
+                                    src='./assets/CGK.svg'
+                                    />
                                 </div>
                                 <div className='ml-2 bg-white pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                                <img
-                                src='./assets/DXT.svg'
-                                />
+                                    <img
+                                    src='./assets/DXT.svg'
+                                    />
                                 </div>
                                 <div className='ml-2 bg-white pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                                <img
-                                src='./assets/TLG.svg'
-                                />
+                                    <img
+                                    src='./assets/TLG.svg'
+                                    />
                                 </div>
                             </div>
                             <div className='flex mt-3'>
@@ -102,24 +105,26 @@ const Tokenomics = () => {
 
                             </div>
                         </div>
-                        <div className=' firstProjectBack bg-FirstProjectBGColor shadow-outline-bottom-first  w-[976px] h-[367px] mt-20 rounded-[39.53px] ml-[24%]'>
-                            <div className='flex flex-row items-center justify-between'>
-                            <div className='flex flex-col'>
-                                <h2 className='firstProjectText text-white text-start w-[460px] ml-16'>First Project on PurpleSale</h2>
-                                <div className=' mt-8 ml-16 items-center justify-center flex flex-row bg-ratedBG w-[223.17px]  rounded-3xl'>
-                                <p className='text-white ratedTextt'>Rated:</p>
-                                <h2 className='text-white alphaText ml-2'>Alpha</h2>
-                                <img
-                                src='./assets/smallStar.svg'
-                                className='ml-1'
-                                />
+                        <div className="flex items-center justify-center">
+                            <div className=' firstProjectBack items-center justify-center bg-FirstProjectBGColor shadow-outline-bottom-first w-[976px] h-[367px] mt-20 rounded-[39.53px]'>
+                                <div className='flex flex-row items-center justify-between'>
+                                <div className='flex flex-col'>
+                                    <h2 className='firstProjectText text-white text-start w-[460px] ml-16'>First Project on PurpleSale</h2>
+                                    <div className=' mt-8 ml-16 items-center justify-center flex flex-row bg-ratedBG w-[223.17px]  rounded-3xl'>
+                                    <p className='text-white ratedTextt'>Rated:</p>
+                                    <h2 className='text-white alphaText ml-2'>Alpha</h2>
+                                    <img
+                                    src='./assets/smallStar.svg'
+                                    className='ml-1'
+                                    />
+                                    </div>
                                 </div>
-                            </div>
-                            <div className=' mt-7'>
-                                <img
-                                src='./assets/FirstStar.svg'
-                                />
-                            </div>
+                                <div className=' mt-7'>
+                                    <img
+                                    src='./assets/FirstStar.svg'
+                                    />
+                                </div>
+                                </div>
                             </div>
                         </div>
                     </div>
