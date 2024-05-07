493de868a3df17b90783bca2093246d5c884d5f9
Feranmiola <88761161+Feranmiola@users.noreply.github.com>
2024-05-07 10:53:16 +0100

    socials update

--- src/components/Roadmap.tsx
+++ src/components/Roadmap.tsx
@@ -8,50 +8,54 @@ const Roadmap = () => {
     <div>
         {full && (
             <div className=''>
-                <div className=' bg-homeBG h-[750px] pt-10'>
+                <div className=' bg-homeBG h-[750px] pt-24'>
                     <div className=' flex items-center justify-center'>
-                        <h2 className='text-white roadmapText text-center'>Roadmap</h2>
+                        <div className="flex items-center justify-center">
+                            <h2 className='text-white roadmapText text-center'>Roadmap</h2>
+                        </div>
                         <img
                         src='./assets/roadmapRing.svg'
-                        className='absolute right-[460px]'
+                        className='absolute ml-[31rem]'
                         />
                     </div>
                     <div>
-                        <div className=' relative flex items-center justify-center mt-44'>
+                        <div className=' relative flex items-center justify-center mt-[11rem]'>
                         <img
                         src='./assets/RoadmapLine.svg'
                         className=''
                         />
                         </div>
 
-                        <div className='absolute flex left-[300px] top-[3350px] '> 
-                        <div className='w-[305px] h-[264px] bg-roadmapContentbg rounded-3xl roadmapShadowBox'>
-                            <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 1</h2>
-                            <ul className=' mt-7 ml-16 '>
-                            <li className='text-white phaseList'>Presale marketing</li>
-                            <li className='text-white phaseList'>Presale on PurpleSale</li>
-                            <li className='text-white phaseList'>Launch</li>
-                            </ul>
-                        </div>
-                        <div className='w-[305px] h-[264px] bg-white rounded-3xl roadmapShadowBox mx-10'>
-                            <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 2</h2>
-                            <ul className=' mt-7 ml-16 '>
-                            <li className='text-black phaseList'>Post launch marketing</li>
-                            <li className='text-black phaseList'>Avedex, dextools, dexscrener trending </li>
-                            <li className='text-black phaseList'>CMC, CG listings</li>
-                            </ul>
-                        
-                        </div>
+                        <div className="flex items-center justify-center">
+                            <div className='absolute flex mt-[4rem]'> 
+                                <div className='w-[305px] h-[264px] bg-roadmapContentbg rounded-3xl roadmapShadowBox'>
+                                    <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 1</h2>
+                                    <ul className=' mt-7 ml-16 '>
+                                    <li className='text-white phaseList'>Presale marketing</li>
+                                    <li className='text-white phaseList'>Presale on PurpleSale</li>
+                                    <li className='text-white phaseList'>Launch</li>
+                                    </ul>
+                                </div>
+                                <div className='w-[305px] h-[264px] bg-white rounded-3xl roadmapShadowBox mx-10'>
+                                    <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 2</h2>
+                                    <ul className=' mt-7 ml-16 '>
+                                    <li className='text-black phaseList'>Post launch marketing</li>
+                                    <li className='text-black phaseList'>Avedex, dextools, dexscrener trending </li>
+                                    <li className='text-black phaseList'>CMC, CG listings</li>
+                                    </ul>
+                                
+                                </div>
 
-                        <div className='w-[305px] h-[264px] bg-roadmapContentbg rounded-3xl roadmapShadowBox'>
-                            <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 3</h2>
-                            <ul className=' mt-7 ml-16 '>
-                            <li className='text-white phaseList'>10,000 holders</li>
-                            <li className='text-white phaseList'>Multichain bridging</li>
-                            <li className='text-white phaseList'>Top tier partnerships</li>
-                            </ul>
-                        
-                        </div>
+                                <div className='w-[305px] h-[264px] bg-roadmapContentbg rounded-3xl roadmapShadowBox'>
+                                    <h2 className='phaseTitle text-white ml-9 mt-10'>Phase 3</h2>
+                                    <ul className=' mt-7 ml-16 '>
+                                    <li className='text-white phaseList'>10,000 holders</li>
+                                    <li className='text-white phaseList'>Multichain bridging</li>
+                                    <li className='text-white phaseList'>Top tier partnerships</li>
+                                    </ul>
+                                
+                                </div>
+                            </div>
 
                         </div>
                     </div>
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -9,33 +9,33 @@ const Socials = () => {
         {full && (
             <div>
                 <div className='bg-homeBG h-[600px]'>
-                <div className='flex-row items-center justify-center'>
-                    <h2 className='socialTitle text-white'>Follow our social media!</h2>
-                    <p className='text-white text-center mt-10 socialText '>Stay Connected for the latest updates and developments within the community.</p>
-                </div>
-                <div className='flex items-center justify-center mt-8'>
-                    <div className='ml-2 bg-roadmapContentbg pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                    <div className='flex'>
-                        <img
-                        src='./assets/telegramIcon.svg'
-                        className='ml-2'
-                        />
-                        <h3 className='text-white textTelegram mt-4 ml-2 mr-2'>Telegram</h3>
+                    <div className="flex flex-col items-center  justify-center">
+                        <div className='flex-row items-center justify-center'>
+                            <h2 className='socialTitle text-white'>Follow our social media!</h2>
+                            <p className='text-white text-center mt-10 socialText '>Stay Connected for the latest updates and developments within the community.</p>
+                        </div>
+                        <div className='flex items-center justify-center mt-8'>
+                            <div className='ml-2 bg-roadmapContentbg pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
+                                <div className='flex'>
+                                    <img
+                                    src='./assets/telegramIcon.svg'
+                                    className='ml-2'
+                                    />
+                                    <h3 className='text-white textTelegram mt-4 ml-2 mr-2'>Telegram</h3>
+                                </div>
+                            </div>
+                            <div className='ml-6 bg-roadmapContentbg pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
+                                <div className='flex'>
+                                    <img
+                                    src='./assets/xIcon.svg'
+                                    className='ml-2'
+                                    />
+                                    <h3 className='text-white textTelegram mt-4 ml-2 mr-2'>X (Twitter)</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <p className='text-white text-center mt-16 copyrightText'>All Rights Reserved © Home of Memes 2024</p>
                     </div>
-                    </div>
-                    <div className='ml-6 bg-roadmapContentbg pl-[9px] pt-[8px] pb-[8px] pr-[16px] shadow-outline-bottom-socials rounded-full'>
-                    <div className='flex'>
-                        <img
-                        src='./assets/xIcon.svg'
-                        className='ml-2'
-                        />
-                        <h3 className='text-white textTelegram mt-4 ml-2 mr-2'>X (Twitter)</h3>
-                    </div>
-                    </div>
-                </div>
-
-                <p className='text-white text-center mt-16 copyrightText'>All Rights Reserved © Home of Memes 2024</p>
-
                 </div>
             </div>
         )}
